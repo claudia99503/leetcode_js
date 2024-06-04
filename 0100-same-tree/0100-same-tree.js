6ef0d843b79083f054b373e582e644adaf48a15e
Min Seo Kim <claudia503@naver.com>
2024-06-04 17:53:19 +0900

    Time: 59 ms (21.4%), Space: 49 MB (60.48%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -25,3 +25,21 @@ var isSameTree = function(p, q) {
     // 왼쪽 서브트리와 오른쪽 서브트리를 재귀적으로 비교
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
+
+/*
+// 함수 isSameTree(p, q):
+    // 두 트리가 모두 null인지 확인
+    만약 p와 q가 모두 null이라면:
+        true 반환
+    
+    // 두 트리 중 하나만 null이거나 두 트리의 값이 다르다면
+    만약 p가 null이거나 q가 null이거나 p의 값이 q의 값과 다르다면:
+        false 반환
+    
+    // 왼쪽 서브트리와 오른쪽 서브트리를 재귀적으로 비교
+    만약 isSameTree(p의 왼쪽 서브트리, q의 왼쪽 서브트리)가 true이고
+        isSameTree(p의 오른쪽 서브트리, q의 오른쪽 서브트리)가 true라면:
+        true 반환
+    그렇지 않다면:
+        false 반환
+*/
