d1328021d413e9868f34c86884b1d7a8cacb80f5
Min Seo Kim <claudia503@naver.com>
2024-06-08 17:12:47 +0900

    Time: 78 ms (39.08%), Space: 58.8 MB (66.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,22 +3,21 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    // 최대 이익을 저장할 변수
-    let maxProfit = 0;
-    // 현재까지의 최소 가격을 저장할 변수, 초기값을 매우 큰 값으로 설정
+    // 최소 가격을 아주 큰 값으로 초기화
     let minPrice = Infinity;
-
-    // 가격 배열을 순회
+    // 최대 이익을 0으로 초기화
+    let maxProfit = 0;
+    
+    // 모든 가격을 순회
     for (let i = 0; i < prices.length; i++) {
-        // 현재 가격이 최소 가격보다 작으면 최소 가격 업데이트
+        // 현재 가격이 최소 가격보다 작으면 최소 가격을 업데이트
         if (prices[i] < minPrice) {
             minPrice = prices[i];
-        // 그렇지 않으면 현재 가격에서 최소 가격을 뺀 값과 최대 이익을 비교하여 최대 이익 업데이트
-        } else {
-            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        } else if (prices[i] - minPrice > maxProfit) { // 현재 가격에서 최소 가격을 뺀 값이 최대 이익보다 크면 최대 이익을 업데이트
+            maxProfit = prices[i] - minPrice;
         }
     }
-
+    
     // 최대 이익 반환
     return maxProfit;
 };
