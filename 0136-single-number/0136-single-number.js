66f8c59695b1810a49247b66b01954877fe7e8cf
Min Seo Kim <claudia503@naver.com>
2024-06-08 20:31:02 +0900

    Time: 64 ms (46.78%), Space: 51.8 MB (48.28%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,9 +3,16 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
+    // 단일 숫자를 저장할 변수 초기화
     let single = 0;
+    
+    // 배열의 각 숫자에 대해 반복
     for (let num of nums) {
+        // 현재 숫자를 단일 변수와 XOR 연산
         single ^= num;
     }
+    
+    // 단일 숫자 반환
     return single;
 };
+
