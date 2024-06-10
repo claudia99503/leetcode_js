ea96f2f8019b29a4865dcd4424a498cd184ac9fc
Min Seo Kim <claudia503@naver.com>
2024-06-10 15:46:05 +0900

    Time: 61 ms (95.63%), Space: 52.9 MB (33.67%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -10,3 +10,33 @@ var titleToNumber = function(columnTitle) {
     }
     return result; // 최종 결과를 반환합니다.
 };
+
+/*
+## 예제 설명:
+
+### 예제 1: columnTitle = "A"
+
+1. 문자 'A': 
+   - charValue = 1
+   - 결과: result = 0 * 26 + 1 = 1
+
+### 예제 2: columnTitle = "AB"
+
+1. 문자 'A': 
+   - charValue = 1
+   - result = 0 * 26 + 1 = 1
+2. 문자 'B': 
+   - charValue = 2
+   - result = 1 * 26 + 2 = 28
+
+### 예제 3: columnTitle = "ZY"
+
+1. 문자 'Z': 
+   - charValue = 26
+   - result = 0 * 26 + 26 = 26
+2. 문자 'Y': 
+   - charValue = 25
+   - result = 26 * 26 + 25 = 701
+
+이 코드는 문자열을 순회하면서 각 문자를 26진수로 변환하여 최종 열 번호를 계산합니다.
+*/
