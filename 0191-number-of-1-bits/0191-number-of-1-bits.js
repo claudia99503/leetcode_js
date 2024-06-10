d38ff632b1d010a2c2fcd94330f37363741f21f4
Min Seo Kim <claudia503@naver.com>
2024-06-10 16:00:42 +0900

    Time: 65 ms (18.5%), Space: 49.1 MB (77.87%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -5,8 +5,11 @@
 var hammingWeight = function(n) {
     let count = 0;
     while (n !== 0) {
-        count += n & 1; // 현재 비트가 1인지 확인하고, 1이면 count를 증가시킵니다.
-        n >>>= 1; // n을 오른쪽으로 1비트 이동하여 다음 비트를 확인합니다.
+        if (n % 2 === 1) { // n을 2로 나눈 나머지가 1이면 마지막 비트는 1입니다.
+            count++; // 비트가 1이면 count를 증가시킵니다.
+        }
+        n = Math.floor(n / 2); // n을 2로 나누어 다음 비트를 확인합니다.
     }
     return count;
 };
+
