6ecb46ff0b43cc0fd89f042a710dacc976478a38
Min Seo Kim <claudia503@naver.com>
2024-06-14 01:29:27 +0900

    Time: 111 ms (43.38%), Space: 72.5 MB (33.96%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -20,3 +20,13 @@ var containsNearbyDuplicate = function(nums, k) {
     // 모든 요소를 검사한 후에도 조건을 만족하는 쌍이 없다면 false를 반환합니다.
     return false;
 };
+
+/*
+1. 함수 `containsNearbyDuplicate`를 정의하고, 매개변수로 정수 배열 `nums`와 정수 `k`를 받는다.
+2. 새로운 빈 Map을 생성하여 변수 `map`에 저장한다.
+3. 배열 `nums`의 각 요소에 대해 다음을 반복한다:
+    3.1. 현재 요소 `nums[i]`가 Map에 이미 존재하는지 확인한다.
+        - 존재하고, 현재 인덱스 `i`와 Map에 저장된 인덱스 차이가 `k` 이하이면, `true`를 반환한다.
+    3.2. 현재 요소 `nums[i]`와 그 인덱스 `i`를 Map에 추가한다.
+4. 모든 요소를 검사한 후에도 조건을 만족하는 쌍이 없다면, `false`를 반환한다.
+*/
