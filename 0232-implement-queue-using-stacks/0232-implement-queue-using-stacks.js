c5b01a9d376fdb8ceafb7894c88617aaaa8bc1f9
Min Seo Kim <claudia503@naver.com>
2024-06-16 23:20:33 +0900

    Time: 55 ms (32.42%), Space: 48.4 MB (83.53%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -2,6 +2,11 @@ var MyQueue = function() {
     this.stack1 = [];
     this.stack2 = [];
 };
+/*
+함수 MyQueue():
+    stack1을 빈 리스트로 초기화
+    stack2을 빈 리스트로 초기화
+/*
 
 /** 
  * @param {number} x
@@ -10,6 +15,10 @@ var MyQueue = function() {
 MyQueue.prototype.push = function(x) {
     this.stack1.push(x);
 };
+/*
+함수 push(x):
+    x를 stack1에 푸시
+*/
 
 /**
  * @return {number}
@@ -22,6 +31,13 @@ MyQueue.prototype.pop = function() {
     }
     return this.stack2.pop();
 };
+/*
+함수 pop():
+    만약 stack2가 비어있다면:
+        stack1이 빌 때까지:
+            stack1에서 stack2로 최상단 요소를 이동
+    stack2에서 최상단 요소를 팝하고 반환
+*/
 
 /**
  * @return {number}
@@ -34,6 +50,13 @@ MyQueue.prototype.peek = function() {
     }
     return this.stack2[this.stack2.length - 1];
 };
+/*
+함수 peek():
+    만약 stack2가 비어있다면:
+        stack1이 빌 때까지:
+            stack1에서 stack2로 최상단 요소를 이동
+    stack2의 최상단 요소를 반환 (팝하지 않음)
+*/
 
 /**
  * @return {boolean}
@@ -41,6 +64,11 @@ MyQueue.prototype.peek = function() {
 MyQueue.prototype.empty = function() {
     return this.stack1.length === 0 && this.stack2.length === 0;
 };
+/*
+함수 empty():
+    stack1과 stack2가 모두 비어있으면 True 반환
+    그렇지 않으면 False 반환
+*/
 
 // Example usage:
 const myQueue = new MyQueue();
