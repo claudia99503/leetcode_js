e803384ce5b4e0080a5181bbb746a32e8e90caff
Min Seo Kim <claudia503@naver.com>
2024-06-20 23:28:16 +0900

    Time: 46 ms (96.94%), Space: 50.3 MB (98.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,13 +3,16 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let n = nums.length;
-    let expectedSum = (n * (n + 1)) / 2;
-    let actualSum = 0;
+    let n = nums.length; // 배열의 길이 n을 변수 n에 저장
+    let expectedSum = (n * (n + 1)) / 2; // 0부터 n까지의 숫자의 합을 계산하여 expectedSum에 저장
+    let actualSum = 0; // nums 배열의 실제 합을 저장할 변수 actualSum을 0으로 초기화
     
+    // nums 배열의 모든 요소를 합산하여 actualSum에 저장
     for (let i = 0; i < nums.length; i++) {
         actualSum += nums[i];
     }
     
+    // 예상되는 합에서 실제 합을 빼서 누락된 숫자를 반환
     return expectedSum - actualSum;
 };
+
