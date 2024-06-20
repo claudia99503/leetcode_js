c3da89563acfab9d78e03fb504ddff3394668de2
Min Seo Kim <claudia503@naver.com>
2024-06-20 23:29:05 +0900

    Time: 44 ms (98.36%), Space: 51 MB (83.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -16,3 +16,14 @@ var missingNumber = function(nums) {
     return expectedSum - actualSum;
 };
 
+/*
+1. 함수 `missingNumber`를 정의하고, 매개변수로 배열 `nums`를 받습니다.
+2. 변수 `n`에 배열 `nums`의 길이를 저장합니다.
+3. 변수 `expectedSum`에 0부터 n까지의 모든 숫자의 합을 계산하여 저장합니다.
+   - 계산식: `expectedSum = (n * (n + 1)) / 2`
+4. 변수 `actualSum`을 0으로 초기화합니다.
+5. 배열 `nums`의 모든 요소를 합산하여 `actualSum`에 저장합니다.
+   - 반복문을 사용하여 `nums` 배열의 각 요소를 순회하며 `actualSum`에 더합니다.
+6. 예상되는 합(`expectedSum`)에서 실제 합(`actualSum`)을 빼서 누락된 숫자를 계산합니다.
+7. 누락된 숫자를 반환합니다.
+*/
