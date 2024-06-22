fe56bdf6ac11d43e85041c17370492c0eedbe63a
Min Seo Kim <claudia503@naver.com>
2024-06-22 14:27:25 +0900

    Time: 160 ms (35.48%), Space: 76 MB (53.64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,33 +10,24 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    // 리스트가 비어있으면 true 반환
-    if (!head) return true;
-    
-    // 링크드 리스트의 중간을 찾기 위해 slow와 fast 포인터 사용
-    let slow = head, fast = head;
-    while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
+    // 연결 리스트의 값을 배열에 복사
+    let vals = []; // 값들을 저장할 배열 초기화
+    let current = head; // 현재 노드를 가리킬 포인터 초기화
+    while (current !== null) {
+        vals.push(current.val); // 현재 노드의 값을 배열에 추가
+        current = current.next; // 다음 노드로 이동
     }
     
-    // 링크드 리스트의 후반부를 반전시킴
-    let prev = null;
-    while (slow) {
-        let temp = slow.next;
-        slow.next = prev;
-        prev = slow;
-        slow = temp;
+    // 배열이 회문인지 확인
+    let left = 0; // 왼쪽 포인터 초기화
+    let right = vals.length - 1; // 오른쪽 포인터 초기화
+    while (left < right) {
+        if (vals[left] !== vals[right]) { // 왼쪽 값과 오른쪽 값이 다르면
+            return false; // 회문이 아니므로 false 반환
+        }
+        left++; // 왼쪽 포인터를 오른쪽으로 이동
+        right--; // 오른쪽 포인터를 왼쪽으로 이동
     }
-    
-    // 첫 번째 반과 반전된 두 번째 반을 비교
-    let left = head, right = prev;
-    while (right) {
-        if (left.val !== right.val) return false;
-        left = left.next;
-        right = right.next;
-    }
-    
-    // 모든 노드가 일치하면 true 반환
-    return true;
+    return true; // 모든 값이 같다면 회문이므로 true 반환
 };
+
