be65867f6e68a079eff7650cd100458fb25a400a
Min Seo Kim <claudia503@naver.com>
2024-06-26 23:13:57 +0900

    Time: 94 ms (70.59%), Space: 57.3 MB (68.08%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.prefixSum = [0];
+    for (let i = 0; i < nums.length; i++) {
+        this.prefixSum.push(this.prefixSum[i] + nums[i]);
+    }
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    return this.prefixSum[right + 1] - this.prefixSum[left];
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
