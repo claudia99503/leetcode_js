86d2021f746850ff08df61e0f9f6ffed65b37e8a
Min Seo Kim <claudia503@naver.com>
2024-06-27 23:47:19 +0900

    Time: 137 ms (90.71%), Space: 58.4 MB (76.69%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -3,11 +3,14 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
+    // n이 0 이하인 경우, 3의 거듭제곱이 될 수 없으므로 false 반환
     if (n <= 0) return false;
     
+    // n이 3으로 나누어떨어질 동안 계속해서 3으로 나눔
     while (n % 3 === 0) {
         n /= 3;
     }
     
+    // n이 1이 되면 true 반환, 그렇지 않으면 false 반환
     return n === 1;
 };
