0056d5776a75cf714d4e450589076deb8643d003
Min Seo Kim <claudia503@naver.com>
2024-06-29 22:34:51 +0900

    Time: 86 ms (8.12%), Space: 52.3 MB (58.05%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n <= 0) {
+        return false;
+    }
+    
+    if (n === 1) {
+        return true;
+    }
+    
+    while (n % 4 === 0) {
+        n /= 4;
+    }
+    
+    return n === 1;
+};
