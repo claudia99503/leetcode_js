3d728eaf72a9f133ffa342a0b0a4802916955119
Min Seo Kim <claudia503@naver.com>
2024-06-29 22:35:27 +0900

    Time: 82 ms (15.43%), Space: 52 MB (82.54%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -3,17 +3,21 @@
  * @return {boolean}
  */
 var isPowerOfFour = function(n) {
+    // n이 음수이거나 0이면 4의 거듭제곱이 될 수 없음
     if (n <= 0) {
         return false;
     }
     
+    // n이 1일 경우에는 true를 반환 (4^0 = 1)
     if (n === 1) {
         return true;
     }
     
+    // n이 4의 거듭제곱인지 확인하기 위해 반복문을 사용
     while (n % 4 === 0) {
         n /= 4;
     }
     
+    // 반복문 이후 n이 1이면 4의 거듭제곱, 아니면 아님
     return n === 1;
 };
