f5ee60a65a36ffa4b24b68433cbdd09fbced0815
Min Seo Kim <claudia503@naver.com>
2024-06-30 23:30:14 +0900

    Time: 76 ms (68.04%), Space: 55.7 MB (93.83%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -7,11 +7,14 @@ var reverseString = function(s) {
     let right = s.length - 1;
 
     while (left < right) {
+        // 두 요소를 교환합니다.
         let temp = s[left];
         s[left] = s[right];
         s[right] = temp;
         
+        // 인덱스를 이동합니다.
         left++;
         right--;
     }
 };
+
