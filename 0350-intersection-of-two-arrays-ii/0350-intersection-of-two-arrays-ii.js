9e9d046ac6d04ed32883a043d27108c6a328ffd3
Min Seo Kim <claudia503@naver.com>
2024-07-04 20:53:00 +0900

    Time: 63 ms (29.92%), Space: 51.5 MB (19.53%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -4,8 +4,10 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
+  // 요소들의 출현 횟수를 저장할 빈 객체를 만듭니다.
   let count = {};
-
+  
+  // nums1의 각 요소를 확인하면서 객체에 출현 횟수를 기록합니다.
   for (let num of nums1) {
     if (count[num]) {
       count[num]++;
@@ -14,8 +16,10 @@ var intersect = function(nums1, nums2) {
     }
   }
   
+  // 결과를 저장할 빈 배열을 만듭니다.
   let result = [];
   
+  // nums2의 각 요소를 확인하면서 nums1에서도 같은 수가 존재하고 그 수의 출현 횟수가 0보다 큰 경우 결과 배열에 추가합니다.
   for (let num of nums2) {
     if (count[num] > 0) {
       result.push(num);
@@ -23,6 +27,7 @@ var intersect = function(nums1, nums2) {
     }
   }
   
+  // 최종 결과 배열을 반환합니다.
   return result;
 };
 
