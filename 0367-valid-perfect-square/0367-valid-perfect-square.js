d5249bc81add3c7a8bbe66c847c55d012e383c14
Min Seo Kim <claudia503@naver.com>
2024-07-07 22:01:15 +0900

    Time: 41 ms (95.68%), Space: 49 MB (23.62%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -5,11 +5,21 @@
 var isPerfectSquare = function(num) {
     if (num < 1) return false;
     
-    for (let i = 1; i * i <= num; i++) {
-        if (i * i === num) {
+    let left = 1, right = num;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let square = mid * mid;
+        
+        if (square === num) {
             return true;
+        } else if (square < num) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
     
     return false;
 };
+
