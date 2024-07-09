210f1146b4f22940fdc60a5d6eed198cbb45a40c
Min Seo Kim <claudia503@naver.com>
2024-07-09 23:52:55 +0900

    Time: 89 ms (12.69%), Space: 52.7 MB (47.79%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -30,3 +30,18 @@ var canConstruct = function(ransomNote, magazine) {
     return true;
 };
 
+
+/*
+1. magazineCount 객체를 생성한다.
+2. magazine 문자열의 각 문자를 순회하면서:
+   2.1. 만약 magazineCount 객체에 해당 문자가 존재하면:
+        2.1.1. 해당 문자의 개수를 1 증가시킨다.
+   2.2. 만약 magazineCount 객체에 해당 문자가 존재하지 않으면:
+        2.2.1. 해당 문자를 키로 하여 개수를 1로 설정한다.
+3. ransomNote 문자열의 각 문자를 순회하면서:
+   3.1. 만약 magazineCount 객체에 해당 문자가 존재하지 않거나 개수가 0이면:
+        3.1.1. false를 반환한다.
+   3.2. 만약 magazineCount 객체에 해당 문자가 존재하고 개수가 0이 아니면:
+        3.2.1. 해당 문자의 개수를 1 감소시킨다.
+4. 모든 문자를 순회한 후, true를 반환한다.
+*/
