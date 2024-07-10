7f5b6910a6ad9702de6a06aa4833457677cecfbb
Min Seo Kim <claudia503@naver.com>
2024-07-10 23:48:50 +0900

    Time: 103 ms (26.95%), Space: 53.7 MB (54.72%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -5,6 +5,7 @@
 var firstUniqChar = function(s) {
     let charCount = {};
     
+    // 각 문자의 빈도를 계산
     for (let i = 0; i < s.length; i++) {
         let char = s[i];
         if (charCount[char]) {
@@ -14,11 +15,14 @@ var firstUniqChar = function(s) {
         }
     }
     
+    // 첫 번째로 빈도가 1인 문자의 인덱스를 찾음
     for (let i = 0; i < s.length; i++) {
         if (charCount[s[i]] === 1) {
             return i;
         }
     }
     
+    // 반복되지 않는 문자가 없으면 -1 반환
     return -1;
 };
+
