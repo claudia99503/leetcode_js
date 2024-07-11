7e2a416271063bc85d07cc2afa022ec30ded275b
Min Seo Kim <claudia503@naver.com>
2024-07-11 23:15:40 +0900

    Time: 64 ms (32.06%), Space: 51.9 MB (22.79%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,9 +4,10 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    
+    // 각 문자의 빈도를 저장할 객체 생성
     const array = {};
 
+    // 문자열 t의 각 문자의 빈도 증가
     for (let char of t) {
         if (array[char]) {
             array[char]++;
@@ -15,12 +16,14 @@ var findTheDifference = function(s, t) {
         }
     }
 
+    // 문자열 s의 각 문자의 빈도 감소
     for (let char of s) {
         if (array[char]) {
             array[char]--;
         }
     }
 
+    // 빈도가 1인 문자를 찾아 반환
     for (let char in array) {
         if (array[char] === 1) {
             return char;
