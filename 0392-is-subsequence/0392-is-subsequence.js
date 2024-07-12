bde92dd14af6d8f49774f0769bafd50d813a8074
Min Seo Kim <claudia503@naver.com>
2024-07-12 21:33:18 +0900

    Time: 43 ms (96.11%), Space: 49.2 MB (57.24%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -9,19 +9,24 @@ var isSubsequence = function(s, t) {
         return true;
     }
     
-    let i = 0; 
-    let j = 0; 
+    let i = 0; // s의 인덱스
+    let j = 0; // t의 인덱스
     
+    // t를 순회
     while (j < t.length) {
+        // s의 현재 문자가 t의 현재 문자와 일치하면
         if (s[i] === t[j]) {
-            i++; 
+            i++; // s의 다음 문자로 이동
         }
-        
+        // s의 모든 문자를 찾았으면 true를 반환
         if (i === s.length) {
             return true;
         }
-        j++; 
+        j++; // t의 다음 문자로 이동
     }
     
+    // 루프가 끝나면 false를 반환 (s의 모든 문자를 찾지 못함)
     return false;
 };
+
+   
