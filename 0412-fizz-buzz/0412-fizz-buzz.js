7efeb949347b0407cd45fa434a1b86198b553fd3
Min Seo Kim <claudia503@naver.com>
2024-07-16 23:06:09 +0900

    Time: 70 ms (12.27%), Space: 52.1 MB (49.8%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,17 +3,17 @@
  * @return {string[]}
  */
 function fizzBuzz(n) {
-  let answer = [];
-  for (let i = 1; i <= n; i++) {
-    if (i % 15 === 0) {
-      answer.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-      answer.push("Fizz");
-    } else if (i % 5 === 0) {
-      answer.push("Buzz");
-    } else {
-      answer.push(i.toString());
+  let answer = []; // 결과를 저장할 배열 선언
+  for (let i = 1; i <= n; i++) { // 1부터 n까지 순회하는 반복문
+    if (i % 15 === 0) { // i가 15로 나누어 떨어질 때
+      answer.push("FizzBuzz"); // "FizzBuzz"를 배열에 추가
+    } else if (i % 3 === 0) { // i가 3으로 나누어 떨어질 때
+      answer.push("Fizz"); // "Fizz"를 배열에 추가
+    } else if (i % 5 === 0) { // i가 5로 나누어 떨어질 때
+      answer.push("Buzz"); // "Buzz"를 배열에 추가
+    } else { // 위 조건에 해당하지 않을 때
+      answer.push(i.toString()); // 숫자를 문자열로 변환하여 배열에 추가
     }
   }
-  return answer;
+  return answer; // 결과 배열을 반환
 }
