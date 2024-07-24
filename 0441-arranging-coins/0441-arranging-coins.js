32663654c560a5d5c1b8e6e7b7b9184c40b7b98c
Min Seo Kim <claudia503@naver.com>
2024-07-24 21:37:39 +0900

    Time: 87 ms (54.35%), Space: 53.9 MB (19.75%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.js
+++ 0441-arranging-coins/0441-arranging-coins.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var arrangeCoins = function(n) {
-    let i = 1;
-    while (n >= i) {
-        n -= i;
-        i++;
+    let i = 1; // 현재 행 번호를 나타내는 변수 i를 1로 초기화
+    while (n >= i) { // n이 현재 행 번호보다 크거나 같을 때까지 반복
+        n -= i; // n에서 현재 행 번호만큼 코인을 빼줌
+        i++; // 다음 행으로 넘어감
     }
-    return i - 1;
+    return i - 1; // 마지막으로 완성된 행 번호를 반환
 };
+
