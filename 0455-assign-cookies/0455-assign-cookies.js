e61562c400876dcb9cbdcb3669fd5217ba0511c5
Min Seo Kim <claudia503@naver.com>
2024-07-25 15:58:14 +0900

    Time: 75 ms (68.56%), Space: 52.1 MB (43.34%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -4,29 +4,26 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
-    let i = 0;
-    let j = 0;
-    let result = 0;
-    g.sort((a, b) => a - b); // 오름차순 정렬
-    s.sort((a, b) => a - b); // 오름차순 정렬
+    // 탐욕 지수 배열과 쿠키 크기 배열을 오름차순으로 정렬
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
 
+    let i = 0; // 아이들의 인덱스
+    let j = 0; // 쿠키들의 인덱스
+    let contentChildren = 0;
+
+    // 아이들과 쿠키들을 순회
     while (i < g.length && j < s.length) {
-        if (g[i] <= s[j]) {
-            result++;
-            i++;
-            s.splice(j, 1);
-            j = 0;
-        } else {
-            if (j < s.length - 1) {
-                j++  
-            } else {
-                i++;
-                j = 0;
-            }
+        if (s[j] >= g[i]) {
+            // 현재 쿠키가 아이의 탐욕 지수보다 크거나 같으면
+            contentChildren++;
+            i++; // 다음 아이로 이동
         }
+        j++; // 다음 쿠키로 이동
     }
 
-    return result;
+    return contentChildren; // 만족한 아이들의 수 반환
 };
+
 
 
