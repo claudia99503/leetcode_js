fab281da893a06cd9b28dfe8fa37f2060a3b7e53
Min Seo Kim <claudia503@naver.com>
2024-07-28 21:44:03 +0900

    Time: 63 ms (15.79%), Space: 48.9 MB (44.74%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let binaryString = num.toString(2);
+    let newBinaryString = "";
+
+    for (let i = 0; i < binaryString.length; i++) {
+        if (binaryString[i] === "1") {
+            newBinaryString += "0";
+        } else if (binaryString[i] === "0") {
+            newBinaryString += "1";
+        }
+    }
+     
+    let number = parseInt(newBinaryString, 2);
+
+    return number;
+};
+
