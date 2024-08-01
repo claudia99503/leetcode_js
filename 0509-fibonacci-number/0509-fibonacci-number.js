376b8eac17b1bc19637b75edd47a0baff5212be7
Min Seo Kim <claudia503@naver.com>
2024-08-01 16:14:31 +0900

    Time: 53 ms (69.11%), Space: 48.6 MB (57.94%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+};
+
