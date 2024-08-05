77e0e9cb54251593920c94b920fa5681c3613229
Min Seo Kim <claudia503@naver.com>
2024-08-05 11:41:55 +0900

    Time: 57 ms (92.66%), Space: 55.4 MB (34.93%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+    let prev = null;
+    let minDiff = Infinity;
+
+    function inOrderTraversal(node) {
+        if (node === null) {
+            return;
+        }
+
+        inOrderTraversal(node.left);
+
+        if (prev !== null) {
+            minDiff = Math.min(minDiff, node.val - prev);
+        }
+        prev = node.val;
+
+        inOrderTraversal(node.right);
+    }
+
+    inOrderTraversal(root);
+
+    return minDiff;
+};
+
