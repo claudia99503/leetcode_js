88fb3e5e1f055b099a3bf7116bd34879eb3d58cc
Min Seo Kim <claudia503@naver.com>
2024-08-08 11:35:23 +0900

    Time: 59 ms (45.37%), Space: 49 MB (73.36%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    let count = 0;
+    while (n >= 5) {
+        n = Math.floor(n / 5);
+        count += n;
+    }
+    return count;
+};
+
