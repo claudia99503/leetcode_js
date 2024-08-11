f00c8969f0cb9eeb4ffd98d6d3cd9e8d751b3406
Min Seo Kim <claudia503@naver.com>
2024-08-11 10:47:56 +0900

    Time: 78 ms (33.63%), Space: 51.5 MB (17.12%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    let closestSum = nums[0] + nums[1] + nums[2]; 
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const currentSum = nums[i] + nums[left] + nums[right];
+
+            if (currentSum === target) {
+                return currentSum; 
+            }
+
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+
+            if (currentSum < target) {
+                left++;
+            } else {
+                right--; 
+            }
+        }
+    }
+
+    return closestSum;
+};
+
