fd66c10a4ea30147597c09787d0317281e7d6856
Min Seo Kim <claudia503@naver.com>
2024-08-16 09:00:44 +0900

    Time: 58 ms (67.43%), Space: 50.8 MB (78.87%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    let n = nums.length;
+    
+    let i = n - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+    
+    if (i >= 0) {
+        let j = n - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    
+    let left = i + 1, right = n - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+};
+
