77afced80b4915dcf4fca9964131b882888f86b0
Min Seo Kim <claudia503@naver.com>
2024-09-03 20:10:07 +0900

    Time: 70 ms (92.01%), Space: 60.2 MB (73.31%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let increasing = true;
+    let decreasing = true;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            decreasing = false;
+        }
+        if (nums[i] < nums[i - 1]) {
+            increasing = false;
+        }
+    }
+
+    return increasing || decreasing;
+};
+
