3fee9a00be58f166b19e5c0d4be167e8d5bb272b
Min Seo Kim <claudia503@naver.com>
2024-09-04 09:13:07 +0900

    Time: 81 ms (67.82%), Space: 53 MB (79.88%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let start = 0;
+    let maxLength = 0
+    
+    for (let end = 0; end < s.length; end++) {
+        while (set.has(s[end])) {
+            set.delete(s[start]); 
+            start++; 
+        }
+        
+        set.add(s[end]);
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+    
+    return maxLength;
+};
+
