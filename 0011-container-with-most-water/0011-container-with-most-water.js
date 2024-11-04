c142351bd3365cbbe1781da67e8e64b039e3f7f9
Min Seo Kim <claudia503@naver.com>
2024-11-04 23:02:23 +0900

    Time: 2 ms (78.76%), Space: 58.2 MB (13.83%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const currentArea = Math.min(height[left], height[right]) * width;
+        maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+};
